eb1f2567855732cccfbc86cbda244087dda6068c
Miles Liebren <miles.e.liebren@gmail.com>
2024-11-10 14:29:21 -0600

    Part 2 of LA, parts 1-3

---
+++ SingleResponsibilityPrinciple/URLAsyncProvider.cs
@@ -0,0 +1,24 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    internal class URLAsyncProvider : ITradeDataProvider
+    {
+        private readonly ITradeDataProvider _tradeDataProvider;
+
+        public URLAsyncProvider(ITradeDataProvider tradeDataProvider)
+        {
+            _tradeDataProvider = tradeDataProvider;
+        }
+
+        IEnumerable<string> ITradeDataProvider.GetTradeData()
+        {
+            return Task.Run(() => _tradeDataProvider.GetTradeData()).Result;
+        }
+    }
+}
