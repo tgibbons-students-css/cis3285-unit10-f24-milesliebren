1d47162959e06b56bfcb982d33e8d697bfbb734d
Miles Liebren <miles.e.liebren@gmail.com>
2024-11-10 14:15:49 -0600

    Completed step 1 of LA

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    internal class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        private URLTradeDataProvider _tradeDataProvider;
+
+        public AdjustTradeDataProvider(URLTradeDataProvider uRLTradeDataProvider)
+        {
+            _tradeDataProvider = uRLTradeDataProvider;
+        }
+
+        public IEnumerable<string> GetTradeData()
+        {
+            // Get the raw trade data from the underlying data provider
+            var tradeData = _tradeDataProvider.GetTradeData();
+
+            // Convert each instance of "GBP" to "EUR"
+            var adjustedTradeData = tradeData.Select(trade => trade.Replace("GBP", "EUR"));
+
+            return adjustedTradeData;
+        }
+    }
+}
